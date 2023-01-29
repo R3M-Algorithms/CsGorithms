653916999853ba66da5c85b80afc83c057d61a5d
Renato Moura Martins Medeiros <renamedrj@gmail.com>
2023-01-28 23:35:57 -0300

    Implement Insertion Sort

---
+++ CsGorithms/CsGorithms.UnitTests/Sorting/InsertionSortTests.cs
@@ -0,0 +1,272 @@
+﻿using CsGorithms.Sorting;
+using CsGorithms.UnitTests.Helpers;
+using FluentAssertions;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace CsGorithms.UnitTests.Sorting
+{
+    public class InsertionSortTests
+    {
+        [Fact]
+        public void InsertionSort_ShouldDoNothing_WhenInputIsEmpty()
+        {
+            // Arrange
+            var input = Enumerable.Empty<int>();
+
+            // Act
+            var result = InsertionSort.Sort(input);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortAscending_WhenInputIsNotEmptyAndSortingOrderIsDefault(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInAscendingOrder();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortAscending_WhenInputIsNotEmptyAndSortingOrderIsAscending(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInAscendingOrder();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortDescending_WhenInputIsNotEmptyAndSortingOrderIsDescending(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input, SortingOrder.Descending);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInDescendingOrder();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortAscending_WhenInputIsNotEmptyAndSortingKeyIsInformedSortingOrderIsDefault(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).Select(x => new PersonTest { Age = x }).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input, (x) => x.Age);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInAscendingOrder(z => z.Age);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortAscending_WhenInputIsNotEmptyAndSortingKeyIsInformedSortingOrderIsAscending(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).Select(x => new PersonTest { Age = x }).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input, (x) => x.Age, SortingOrder.Ascending);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInAscendingOrder(z => z.Age);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortDescending_WhenInputIsNotEmptyAndSortingKeyIsInformedSortingOrderIsDescending(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).Select(x => new PersonTest { Age = x }).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input, (x) => x.Age, SortingOrder.Descending);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInDescendingOrder(z => z.Age);
+        }
+
+        /////
+        ///
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortAscending_WhenInputIsNotEmptyAndObjImplementsIComparableAndSortingOrderIsDefault(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).Select(x => new PersonTestComparable { Age = x }).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInAscendingOrder(z => z.Age);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortAscending_WhenInputIsNotEmptyAndObjImplementsIComparableAndSortingOrderIsAscending(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).Select(x => new PersonTestComparable { Age = x }).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInAscendingOrder(z => z.Age);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        [InlineData(128)]
+        [InlineData(256)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        [InlineData(2096)]
+        [InlineData(4192)]
+        public void InsertionSort_ShouldSortDescending_WhenInputIsNotEmptyAndObjImplementsIComparableAndSortingOrderIsDescending(int elemCount)
+        {
+            // Arrange
+            var input = Enumerable.Range(0, elemCount).Select(x => new PersonTestComparable { Age = x }).OrderBy(x => Guid.NewGuid());
+
+            // Act
+            var result = InsertionSort.Sort(input, SortingOrder.Descending);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeInDescendingOrder(z => z.Age);
+        }
+    }
+}
---
+++ CsGorithms/CsGorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿namespace CsGorithms.Sorting
+{
+    public static class InsertionSort
+    {
+        public static IEnumerable<T> Sort<T, TKey>(IEnumerable<T> target
+            , Func<T, TKey> keySelector
+            , SortingOrder sortingOrder = SortingOrder.Ascending)
+        {
+            var result = new List<T>(target);
+            var isSorted = SortHelpers.IsOrdered(sortingOrder);
+            var comparer = Comparer<TKey>.Default.Compare;
+            for (int i = 1; i < result.Count; i++)
+            {
+                int j = i - 1;
+                var pivot = result[i];
+                while (j >= 0 && !isSorted(comparer(keySelector(result[j]), keySelector(pivot))))
+                {
+                    result[j+1] = result[j];
+                    j--;
+                }
+                result[j + 1] = pivot;
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<T> Sort<T>(IEnumerable<T> target, SortingOrder sortingOrder = SortingOrder.Ascending)
+        {
+            return Sort(target, (x) => x, sortingOrder);
+        }
+    }
+}
